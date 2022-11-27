22c2581566828c74203aa59184477df9d8cc470b
BenMacklin <benjaminmacklin92@gmail.com>
2022-11-27 18:29:39 -0500

    Routes updated and controller actions created except destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,43 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
   def new
+    binding.pry
+    @checked = params[:checked].present?
     @user = User.new
   end
 
   def create
+    binding.pry
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user), notice: "User created!"
+      if should_stay?
+        redirect_to new_user_path(checked: true)
+      else
+        redirect_to user_path(@user), notice: "User created!"
+      end
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "User updated!"
+    else
+      render :edit
     end
   end
 
@@ -22,4 +45,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def should_stay?
+    params[:should_stay].present?
+  end
+
 end
