8c6e3767dad390a8dc06a14b19dd5fec74711c71
BenMacklin <benjaminmacklin92@gmail.com>
2022-11-27 15:29:53 -0500

    Adds show for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -7,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path(@user), notice: "User created!"
+      redirect_to user_path(@user), notice: "User created!"
     else
       render :new
     end
